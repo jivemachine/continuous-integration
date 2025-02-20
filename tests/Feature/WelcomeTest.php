404023dc2668c55bc047a7b9d91fa65e0e3c26cd
Jeremy <the.jeremy.cobb@gmail.com>
2025-02-20 15:57:39 -0600

    Update WelcomeTest to remove landing page visit count test and adjust user factory count

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -2,19 +2,8 @@
 
 use App\Models\User;
 use Illuminate\Support\Facades\Redis;
-
-it('increments the page count for each visit', function () {
-    Redis::del('landing-page-views');
-
-    $this->get('/');
-    $this->get('/');
-    $this->get('/');
-
-    expect(Redis::get('landing-page-views'))->toEqual(3);
-});
-
 it('provides users in random paginated order', function () {
-    $users = User::factory(3)->create();
+    $users = User::factory(4)->create();
 
     $users = collect($this->get('/')->viewData('users')->items())
         ->merge($this->get('/?page=2')->viewData('users')->items());
