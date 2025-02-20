07796b5dc152025cdac7e4da0dcba46ba11073be
Jeremy <the.jeremy.cobb@gmail.com>
2025-02-20 16:21:18 -0600

    Update landing page route to track views and randomize user pagination

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
-    $users = User::paginate(2);
-    return view('welcome', ['users' => $users]);
+    Redis::incr('landing-page-views');
+    $seed = Session::remember('users.seed', fn () => rand(0, 100));
+
+    return view('welcome', [
+        'users' => User::inRandomOrder($seed)->paginate(3),
+    ]);
 });
 
 Route::middleware([
